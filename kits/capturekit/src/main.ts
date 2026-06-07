ce19d409fc34329a557703225abc6380d5aa7ed0
book诗意 <tech.gosling0d@icloud.com>
2026-06-08 00:40:26 +0800

    修复样式问题

--- kits/capturekit/src/main.ts
+++ kits/capturekit/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import "@weblink/tokens/index.css";
 import App from "./App.vue";
 import "./styles/base.css";
 import { logger } from "./infrastructure/logging/logger";
--- kits/wirelesskit/src/app/main.ts
+++ kits/wirelesskit/src/app/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@weblink/tokens/index.css";
 import "@/app/styles/tokens.css";
 import "@/app/styles/base.css";
 
